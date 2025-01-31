dfe9c8792066aa4d027707b8a3008c85e0d79c08
Guillaume Gaisne <guillaume.gaisne@esrf.fr>
2025-01-31 15:13:02 +0100

    Create some passing tests

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -8,6 +8,10 @@ from nomad_damask_parser.parsers.myparser import MyParser
 def test_parse_file():
     parser = MyParser()
     archive = EntryArchive()
-    parser.parse('tests/data/example.out', archive, logging.getLogger())
+    parser.parse(
+        'tests/data/4grains2x4x3_compressionY.hdf5',
+        archive,
+        logging.getLogger()
+    )
 
-    assert archive.results.material.elements == ['H', 'O']
+    assert archive.results.method.simulation.program_name == 'DAMASK'
--- tests/schema_packages/test_schema.py
+++ tests/schema_packages/test_schema.py
@@ -1,11 +1,10 @@
 import os.path
 
-from nomad.client import normalize_all, parse
+from nomad.client import parse
 
 
 def test_schema():
     test_file = os.path.join('tests', 'data', 'test.archive.yaml')
     entry_archive = parse(test_file)[0]
-    normalize_all(entry_archive)
 
-    assert entry_archive.data.message == 'Hello Markus!'
+    assert entry_archive.data.code_version == '3.0.1'
