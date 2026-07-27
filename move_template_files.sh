10c7ef9dd5c308019a3bd11fa6471171107f3c23
Guillaume-Gaisne <Guillaume-Gaisne@users.noreply.github.com>
2026-07-27 16:10:09 +0000

    chore: accept new Cruft update

--- move_template_files.sh
+++ move_template_files.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-rsync -avh nomad-DAMASK_parser/ .
-rm -rfv nomad-DAMASK_parser
+if ! command -v rsync >/dev/null 2>&1; then
+  echo "rsync required, but not installed!"
+  exit 1
+else
+  rsync -avh DAMASK_parser/ .
+  rm -rfv DAMASK_parser
+fi
